b28522f837b8755a57f96d7c6cebc8a7fbb3a5e6
Krzysztof Grzelak <maginowo@gmail.com>
2025-05-12 11:10:15 +0200

    Added add function and base for everything

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <string.h>
 
-int main() {
-    printf("Hellow");
+int main(int argc, char const *argv[]) {
+    int number1, number2, result;
+
+    number1 = atoi(argv[1]);
+    number2 = atoi(argv[3]);
+
+    if (strcmp(argv[2], "add") == 0) {
+        result = number1 + number2;
+        printf("%d + %d = %d\n", number1, number2, result);
+    }
+
+
+
     return 0;
 }
